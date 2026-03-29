03f5ac938b55110db9f35d62434eb2cb281803b7
MukeshP1234 <mukeshpm.cd24@rvce.edu.in>
2026-03-29 12:51:53 +0530

    Add files via upload

---
+++ Mukesh_1RV24CD034/Assignment-3/Assignment-5/Task3.js
@@ -0,0 +1,9 @@
+const p1 = new Promise(res => setTimeout(() => res("User loaded"), 1000));
+const p2 = new Promise((res, rej) => setTimeout(() => rej("Server error"), 2000));
+const p3 = new Promise(res => setTimeout(() => res("Products loaded"), 3000));
+
+Promise.allSettled([p1, p2, p3]).then(results => {
+    results.forEach(r => {
+        console.log(r.status, "-", r.value || r.reason);
+    });
+});
